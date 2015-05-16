e33a22c0f630c424f835ceb4497cc4359da98e5b
Alex <sdfh32r98fsd23@wed.sd>
2015-05-16 19:21:48 +0200

    Made and implemented Class GameObject.cs Implemented Class EnvironmentController.cs with Method to initilise a map and load objects with it.

---
+++ EmodiaQuest/EmodiaQuest/EmodiaQuest/Core/EnvironmentController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace EmodiaQuest.Core
+{
+    class EnvironmentController
+    {
+        // TODO: Controll the whole environment, is used for getting easy access of everything wich is static, needs to be rendered or has collsion
+        // will have a lot of lists with items
+
+        public Texture2D map;
+        public Color[] colors1D;
+        public Color[,] colors2D;
+
+        List<GameObject> ground, wall, items, accessoires, buildings;
+
+        /// <summary>
+        /// Creates a new map from a pixelmap
+        /// <param name="map">A Texture2D with loaded map-picture.</param>
+        /// </summary>
+        public void createMap(Texture2D map)
+        {
+            this.map = map;
+
+            colors1D = new Color[map.Width * map.Height];
+            map.GetData(colors1D);
+            
+            colors2D = new Color[map.Width, map.Height];
+            for (int x = 0; x < map.Width; x++)
+            {
+                for (int y = 0; y < map.Height; y++)
+                {
+                    colors2D[x, y] = colors1D[x + y * map.Width];
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Method for fillig an object list with objects including their models and positions
+        /// <param name="objList">List, that takes all the new objects.</param>
+        /// <param name="obj">What type of objects should be used?</param>
+        /// <param name="color">On wich pixel-color is used to display the objects?</param>
+        /// <param name="height">Integer for placing the object in Z-axis</param>
+        /// </summary>
+        public void insertObj (List<GameObject> objList, Model model, Color color, int height)
+        {
+            for (int i = 0; i < map.Width; i++)
+            {
+                for (int j = 0; j < map.Height; j++)
+                {
+                    if (colors2D[i, j] == color)
+                        objList.Add(new GameObject(model, new Vector3(i, height, j)));
+                }
+            }
+        }
+    }
+}
---
+++ EmodiaQuest/EmodiaQuest/EmodiaQuest/Core/GameObject.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace EmodiaQuest.Core
+{
+    
+    public class GameObject
+    {
+        public Vector3 position;
+        public Vector3 rotation;
+        public float scale;
+        public Model model;
+        public Matrix projektion, view;
+
+        /// <summary>
+        /// Creates a new map from a pixelmap
+        /// <param name="model">A model for the .</param>
+        /// <param name="view">A view-matrix.</param>
+        /// </summary>
+        public GameObject(Model model, Vector3 position)
+        {
+            this.model = model;
+            this.position = position;
+        }
+
+        /*
+        /// <summary>
+        /// Creates a new map from a pixelmap
+        /// <param name="projektion">A projektion-matrix.</param>
+        /// <param name="view">A view-matrix.</param>
+        /// <param name="rotation">Rotation vector.</param>
+        /// <param name="scale">Used for scaling the object.</param>
+        /// </summary>
+        public void Draw(Matrix projektion, Matrix view, Vector3 rotation, float scale)
+        {
+            this.projektion = projektion;
+            this.view = view;
+            this.rotation = rotation;
+            this.scale = scale;
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect effects in mesh.Effects)
+                {
+                    effects.View = view;
+                    effects.Projection = projektion;
+                    effects.EnableDefaultLighting();
+                    effects.World = Matrix.CreateScale(scale) * Matrix.CreateFromYawPitchRoll(rotation.X, rotation.Y, rotation.Z) * Matrix.CreateTranslation(position);
+                }
+                mesh.Draw();
+            }
+        }
+        */
+    }
+}
