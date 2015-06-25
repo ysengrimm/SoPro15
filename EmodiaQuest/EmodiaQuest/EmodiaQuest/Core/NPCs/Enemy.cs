2b3ceba7e4bc6736ef05b00686eeaafbcbd094ed
Zarnosch <janoszwei@yahoo.de>
2015-06-25 22:48:53 +0200

    some changes to the enemy

--- EmodiaQuest/EmodiaQuest/EmodiaQuest/Core/NPCs/Enemy.cs
+++ EmodiaQuest/EmodiaQuest/EmodiaQuest/Core/NPCs/Enemy.cs
@@ -16,24 +16,24 @@ namespace EmodiaQuest.Core.NPCs
     abstract class Enemy
     {
         // The Model
-        abstract private Model enemyModel, standingM, walkingM, jumpingM, swordfightingM, bowfightingM, runningM, idleM;
+        private Model enemyModel, standingM, walkingM, jumpingM, swordfightingM, bowfightingM, runningM, idleM;
         // Skinning Data
-        abstract private SkinningData standingSD, walkingSD, jumpingSD, swordfightingSD, bowfightingSD, runningSD, idleSD;
+        private SkinningData standingSD, walkingSD, jumpingSD, swordfightingSD, bowfightingSD, runningSD, idleSD;
         // The animation Player
-        abstract private AnimationPlayer standingAP, walkingAP, jumpingAP, swordfightingAP, bowfightingAP, runningAP, idleAP;
+        private AnimationPlayer standingAP, walkingAP, jumpingAP, swordfightingAP, bowfightingAP, runningAP, idleAP;
         // The animation Clips, which will be used by the model
-        abstract private AnimationClip standingC, walkingC, jumpingC, swordfightingC, bowfightingC, runningC, idleC;
+        private AnimationClip standingC, walkingC, jumpingC, swordfightingC, bowfightingC, runningC, idleC;
         // The Bone Matrices for each animation
-        abstract private Matrix[] blendingBones, standingBones, walkingBones, jumpingBones, swordfightingBones, bowfightingBones, runningBones, idleBones;
+        private Matrix[] blendingBones, standingBones, walkingBones, jumpingBones, swordfightingBones, bowfightingBones, runningBones, idleBones;
         // The playerState, which will be needed to update the right animation
-        abstract public EnemyState EnemyState = EnemyState.Standing;
-        abstract public EnemyState LastEnemyState = EnemyState.Standing;
+        public EnemyState EnemyState = EnemyState.Standing;
+        public EnemyState LastEnemyState = EnemyState.Standing;
 
         // Enemystats
-        abstract public Vector2 Position;
-        abstract public float Hp;
-        abstract public float Armor;
-        abstract public float MovementSpeed;
+        public Vector2 Position;
+        public float Hp;
+        public float Armor;
+        public float MovementSpeed;
 
 
         // Methods
