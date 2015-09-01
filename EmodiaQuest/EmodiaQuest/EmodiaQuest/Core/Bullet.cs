d3a8a89f70d203de87e07d42d15087b04ecd0e87
Mikula94 <alex.mikulinski@st.ovgu.de>
2015-09-01 20:52:01 +0200

    Bullet class implemendet. Collision is missing.

---
+++ EmodiaQuest/EmodiaQuest/EmodiaQuest/Core/Bullet.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace EmodiaQuest.Core
+{
+    public class Bullet
+    {
+        private Model bulletModel;
+        public Vector2 bulletPosition;
+        public Vector2 bulletMovement;
+        public bool isActive;
+        private float moveSpeed = 10f;
+
+        public Bullet(Model bulletModel)
+        {
+            this.bulletPosition = Player.Instance.Position;
+            this.bulletModel = bulletModel;
+            isActive = false;
+        }
+
+        public void ShootBullet(Vector2 bulletMovement)
+        {
+            isActive = true;
+            this.bulletMovement = bulletMovement;
+            bulletMovement.Normalize();
+        }
+
+        public void Update(GameTime gameTime, CollisionHandler collisionHandler)
+        {
+            float elapsedTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            CollisionHandling(collisionHandler);
+            bulletPosition += (bulletMovement * moveSpeed * elapsedTime);
+        }
+
+        public void CollisionHandling(CollisionHandler collisionHandler)
+        {
+            // Collision with walls
+
+            // Collision with enemies
+        }
+
+        public void Kill()
+        {
+            isActive = false;
+            bulletPosition = Player.Instance.Position;
+        }
+
+        public void Draw(Matrix world, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in bulletModel.Meshes)
+            {
+                foreach (BasicEffect effect in mesh.Effects)
+                {
+                    effect.EnableDefaultLighting();
+                    effect.World = world * Matrix.CreateScale(0.3f) * Matrix.CreateTranslation(new Vector3(bulletPosition.X, 1.3f, bulletPosition.Y));
+                    effect.View = view;
+                    effect.Projection = projection;
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
